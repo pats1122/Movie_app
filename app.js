297798f7ff0b8325e425792de631577b01f8a17a
pats1122 <shalupatwari1122@gmail.com>
2021-05-04 23:31:38 +0530

    added enviornment vars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const request = require('request')
 const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
 app.set("view engine", "ejs")
 app.use('/public', express.static('public'))
 
@@ -14,7 +16,7 @@ app.get('/dummy', (req, res) => {
 app.get('/result', (req,res) => {
     //console.log('req.query')
     //res.send(`You have searched for ${req.query.movie_name}`)
-    const url = `http://www.omdbapi.com/?apikey=d6892854&s=${req.query.movie_name}`
+    const url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${req.query.movie_name}`
     request(url, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             const data = JSON.parse(body)
@@ -26,7 +28,7 @@ app.get('/result', (req,res) => {
     })
 })
 app.get('/result/:id', (req, res) => {
-    const url = `http://www.omdbapi.com/?apikey=d6892854&i=${req.params.id}`
+    const url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${req.params.id}`
     request(url, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             const data = JSON.parse(body)
